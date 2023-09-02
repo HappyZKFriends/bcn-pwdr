3add2d5b75a3c9b8eb13444ad58c6f685caf5372
r0qs <rodrigo.saramago@ethereum.org>
2023-09-02 17:41:47 +0200

    Fix code format

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -6,44 +6,44 @@ use powdr_number::GoldilocksField;
 
 #[derive(Subcommand)]
 enum ZKBeaconCommands {
-	Run,
+    Run,
 }
 #[derive(Subcommand)]
 enum Commands {
-	ZKVerifier {
-		#[command(subcommand)]
-		command: ZKBeaconCommands,
-	},
+    ZKVerifier {
+        #[command(subcommand)]
+        command: ZKBeaconCommands,
+    },
 }
 
 #[derive(Parser)]
 #[command(version, about)]
 struct Cli {
-	#[command(subcommand)]
-	command: Commands,
+    #[command(subcommand)]
+    command: Commands,
 }
 
 // TODO: add light client module
 // TODO: add retrieve beacon chain block header
 pub fn run_cli() {
-	let cli = Cli::parse();
-	match &cli.command {
-		Commands::ZKVerifier { command } => {
-			match command {
-				ZKBeaconCommands::Run => {
-					// TODO: call verifier implementation
-					let inputs: Vec<GoldilocksField> = vec![];
-					let circuit_file = "./circuits/asm/arith384.asm";
-					compile_pil_or_asm(
-						circuit_file,
-						inputs,
-						&mktemp::Temp::new_dir().unwrap(), //TODO: save generated witnesses and constants
-						true,
-						None //TODO: allow use of other backends, e.g. Halo2
-					)
-					.unwrap();
-				}
-			}
-		}
-	};
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::ZKVerifier { command } => {
+            match command {
+                ZKBeaconCommands::Run => {
+                    // TODO: call verifier implementation
+                    let inputs: Vec<GoldilocksField> = vec![];
+                    let circuit_file = "./circuits/asm/arith384.asm";
+                    compile_pil_or_asm(
+                        circuit_file,
+                        inputs,
+                        &mktemp::Temp::new_dir().unwrap(), //TODO: save generated witnesses and constants
+                        true,
+                        None, //TODO: allow use of other backends, e.g. Halo2
+                    )
+                    .unwrap();
+                }
+            }
+        }
+    };
 }
