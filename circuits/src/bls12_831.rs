059f5da0670fc999a228e9c4fe76136f00f07f2d
isk <0xisk>
2023-09-03 09:43:05 +0200

    push sha256 block_hash verifier

--- circuits/src/bls12_831.rs
+++ circuits/src/bls12_831.rs
@@ -8,6 +8,7 @@ use runtime::get_prover_input;
 #[no_mangle]
 fn main() {
 	verifyBLS12_831();
+}
 
 fn verifyBLS12_831() {
 	// TODO: import asm/arith384.asm and implement the bls signature verification
